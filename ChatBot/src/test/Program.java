57023e4e929acbc1b19bbf71935b052760413a94
Ravi kumar <Ravi kumar>
2017-02-17 01:12:27 +0530

    Added initial aiml support

--- ChatBot/src/test/Program.java
+++ ChatBot/src/test/Program.java
@@ -1,5 +1,7 @@
 package test;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -7,7 +9,7 @@ import org.json.JSONObject;
 import org.omg.CORBA.PRIVATE_MEMBER;
 import org.json.JSONArray;
 import org.json.JSONException;
- 
+import org.alicebot.ab.*; 
 
 public class Program
 {
@@ -43,7 +45,24 @@ public class Program
 			}
 			
 			else
-				 sc = new SpellCheck(question);
+			{
+				PrintStream myFile= new PrintStream("dummy.txt");
+				
+				
+				System.setOut(myFile);
+				
+				String botname="super";
+				String path="c:/ab"; 
+				Bot bot = new Bot(botname, path); 
+				Chat chatSession = new Chat(bot); 
+			
+				String request = question;
+				String response = chatSession.multisentenceRespond(request); 
+				
+				System.setOut(System.out);
+				System.out.println(response); 
+						
+			}
 				
 			
 		}
--- ChatBot/src/test/SpellCheck.java
+++ ChatBot/src/test/SpellCheck.java
@@ -1,28 +1,52 @@
+ // // This sample uses the Apache HTTP client from HTTP Components (http://hc.apache.org/httpcomponents-client-ga/)
 package test;
-
-import java.io.IOException;
-import org.apache.httpcomponent.*;
-import org.apache.commons.httpclient.HttpClient;
+import java.net.URI;
+import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
-import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.HttpClients;
-import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.util.EntityUtils;
 
-import org.apache.http.impl.client.HttpClientBuilder;
+public class SpellCheck
+{
+    public SpellCheck(String text)
+    {
+        HttpClient httpclient = HttpClients.createDefault();
 
-class SpellCheck{
-	public SpellCheck(String queryString) throws ClientProtocolException, IOException
-	{
-		//CloseableHttpClient httpClient = HttpClientBuilder.create().build();		
-		CloseableHttpClient httpclient = HttpClients.createDefault();
-		String url = "https://api.cognitive.microsoft.com/bing/v5.0/spellcheck/?" + queryString;
+        try
+        {
+            URIBuilder builder = new URIBuilder("https://api.cognitive.microsoft.com/bing/v5.0/spellcheck/");
 
-		HttpPost httpPost = new HttpPost(url);
+            builder.setParameter("text", text);
+//            builder.setParameter("mode", "{string}");
+//            builder.setParameter("preContextText", "{string}");
+//            builder.setParameter("postContextText", "{string}");
+//            builder.setParameter("mkt", "{string}");
 
-		httpPost.addHeader("Ocp-Apim-Subscription-Key " , "a6a3f0cf5b384bb7a8535db8eb5df295");
+            URI uri = builder.build();
+            HttpPost request = new HttpPost(uri);
+            request.setHeader("Ocp-Apim-Subscription-Key", "713b0991cbac43e48e7ea1537caf1331");
 
-		HttpResponse response = httpclient.execute(httpPost);
-	}
+
+            // Request body
+            StringEntity reqEntity = new StringEntity("{body}");
+            request.setEntity(reqEntity);
+
+            HttpResponse response = httpclient.execute(request);
+            HttpEntity entity = response.getEntity();
+
+            if (entity != null) 
+            {
+                System.out.println(EntityUtils.toString(entity));
+            }
+        }
+        catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+        }
+    }
 }
